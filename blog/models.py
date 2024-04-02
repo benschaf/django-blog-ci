e4d1eb45f39fd26b5d0aa485f5ce799732b5e668
Benjamin Schäfer <benibschafer@gmail.com>
2024-04-02 10:17:51 +0000

    Add excerpt and updated_on fields to Post model

--- blog/models.py
+++ blog/models.py
@@ -12,9 +12,11 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    excerpt = models.TextField(blank = True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
